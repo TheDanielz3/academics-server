991f8540c8b7c4a18e72d8b409eb69df5065b5ef
Daniel Batista <2171836@my.ipleiria.pt>
2024-10-15 23:20:54 +0100

    feat: ficha 3 TPC

--- src/main/java/pt/ipleiria/estg/dei/ei/dae/academics/ejbs/CourseBean.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/academics/ejbs/CourseBean.java
@@ -3,6 +3,7 @@ package pt.ipleiria.estg.dei.ei.dae.academics.ejbs;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import pt.ipleiria.estg.dei.ei.dae.academics.dtos.CourseDTO;
 import pt.ipleiria.estg.dei.ei.dae.academics.entities.Course;
 import pt.ipleiria.estg.dei.ei.dae.academics.entities.Student;
 
@@ -36,4 +37,29 @@ public class CourseBean {
         }
         return course;
     }
+    public Course update(CourseDTO courseDTO){
+        Course course = null;
+        try{
+             course = entityManager.find(Course.class,courseDTO.getCode());
+            if(course == null){
+                throw new RuntimeException("course"+courseDTO.getCode()+"not found!");
+            }
+            course.setName(courseDTO.getName());
+            entityManager.merge(course);
+        }catch (Exception e)
+        {
+
+            throw new RuntimeException("aquiii"+e);
+        }
+        finally {
+            return course;
+
+        }
+    }
+    public Boolean delete(Long code){
+        Course courseToEdit = this.find(code);
+        entityManager.remove(courseToEdit);
+        return true;
+    }
+
 }
--- src/main/java/pt/ipleiria/estg/dei/ei/dae/academics/ws/CourseService.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/academics/ws/CourseService.java
@@ -5,11 +5,8 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import pt.ipleiria.estg.dei.ei.dae.academics.dtos.CourseDTO;
-import pt.ipleiria.estg.dei.ei.dae.academics.dtos.StudentDTO;
 import pt.ipleiria.estg.dei.ei.dae.academics.ejbs.CourseBean;
-import pt.ipleiria.estg.dei.ei.dae.academics.ejbs.StudentBean;
 import pt.ipleiria.estg.dei.ei.dae.academics.entities.Course;
-import pt.ipleiria.estg.dei.ei.dae.academics.entities.Student;
 
 import java.util.List;
 
@@ -27,16 +24,52 @@ public class CourseService {
         return CourseDTO.from(courseBean.findAll());
     }
 
+    @GET// means: to call this endpoint, we need to use the HTTP GET method
+    @Path("/{id}") // means: the relative url path is “/api/students/all”
+    public CourseDTO viewCourse(@PathParam("id") long courseiD) {
+       Course course = courseBean.find(courseiD);
+
+        return CourseDTO.from(course);
+    }
+
     @POST
     @Path("/")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response createNewCouse (CourseDTO courseDTO){
+    public Response createNewCourse (CourseDTO courseDTO){
         courseBean.create(
                 courseDTO.getCode(),
                 courseDTO.getName()
         );
         Course newCourse = courseBean.find(courseDTO.getCode());
-        return Response.status(Response.Status.CREATED) .entity(CourseDTO.from(newCourse)).build();
+        return Response.status(Response.Status.CREATED).entity(CourseDTO.from(newCourse)).build();
+    }
+
+
+    @PUT
+    @Path("/")
+    public Response editCurse (CourseDTO courseDTO){
+        if (courseDTO == null ){
+            return Response.status(Response.Status.NO_CONTENT).build();
+        }
+        Course res = courseBean.update(courseDTO);
+        if(res != null){
+            return Response.status(Response.Status.OK).entity(CourseDTO.from(res)).build();
+        }
+        return Response.status(Response.Status.CONFLICT).build();
+    }
+
+
+    @DELETE
+    @Path("/{id}")
+    public Response removeCourse (@PathParam("id") Long courseiD){
+        if (courseiD == null ){
+            return Response.status(Response.Status.NO_CONTENT).build();
+        }
+       Boolean res = courseBean.delete(courseiD);
+        if(res){
+            return Response.status(Response.Status.OK).build();
+        }
+        return Response.status(Response.Status.CONFLICT).build();
     }
 
 }
